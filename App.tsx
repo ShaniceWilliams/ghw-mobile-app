27c403c6534f5458f0fed0799fe0e0be159310ce
Shanice Williams <shanice.williams@live.co.uk>
2024-11-09 21:46:51 +0000

    initial commit and setup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
@@ -12,9 +13,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: theme.colors.white,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
