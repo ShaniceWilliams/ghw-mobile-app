28575bbed15b9f9332538f23e2cced15c3d57e69
Shanice Williams <shanice.williams@live.co.uk>
2024-11-09 22:59:35 +0000

    Added first content and button

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, Linking, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { theme } from "./theme";
 
 export default function App() {
+  const handleSignup = () => {
+    //Alert.alert("Sign up to GHW", "You are signed up!");
+    Alert.alert("Register for GHW", "Are you interested in participating in MLH GHW?", [
+      {
+        text: "No",
+        style: "destructive",
+        onPress: () => console.log("Not signed up :(")
+      },
+      {
+        text: "Yes",
+        style: "default",
+        onPress: () => Linking.openURL("https://events.mlh.io/events/11595"),
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.headLineText}>Global Hack Week</Text>
+      <Text style={styles.bodyText}>
+        Each month we host a Global Hack Week where you can learn new skills, build your portfolio,
+        attend fun sessions and connect with other hackers in the MLH community.
+      </Text>
+      <Text style={styles.bodyText}>
+        It&apos;s free and easy to get started! Sign up now and get ready to hack!
+      </Text>
+      <TouchableOpacity style={styles.signupButton} onPress={handleSignup}>
+        <Text style={styles.signupButtonText}>Sign Up</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +43,23 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.white,
     flex: 1,
     justifyContent: "center",
+  },
+  headLineText: {
+    ...theme.textVariants.headLine,
+  },
+  bodyText: {
+    ...theme.textVariants.bodyText,
+    width: "80%",
+    marginVertical: 10,
+    textAlign: "center",
+  },
+  signupButton: {
+    backgroundColor: theme.colors.primaryButton,
+    borderRadius: 8,
+    color: theme.colors.white,
+    padding: 16,
+  },
+  signupButtonText: {
+    ...theme.textVariants.buttonTextDark,
   },
 });
--- theme.ts
+++ theme.ts
@@ -1,5 +1,25 @@
 export const theme = {
   colors: {
     white: "#ffffff",
+    black: "#000000",
+    primaryButton: "#5a43f4"
+  },
+  textVariants: {
+    headLine: {
+      fontSize: 32,
+      fontFamily: "System",
+      color: "black",
+      fontWeight: "bold" as "bold",
+    },
+    bodyText: {
+      fontSize: 16,
+      fontFamily: "System",
+      color: "black",
+    },
+    buttonTextDark: {
+      fontSize: 16,
+      fontFamily: "System",
+      color: "white",
+    }
   },
 };
